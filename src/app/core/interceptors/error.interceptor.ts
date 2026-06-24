ec388817843ade58b4f5c840d1be40343cd7d5b8
alescampos2511-lang <alescampos2511@gmail.com>
2026-06-23 19:44:44 -0600

    interceptors  actualizado

---
+++ src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      const message = Array.isArray(error.error?.message)
+        ? error.error.message.join(', ')
+        : error.error?.message || 'Error inesperado';
+
+      return throwError(() => ({
+        status: error.status,
+        message,
+      }));
+    }),
+  );
+};
