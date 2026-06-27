4a8301e5ad9741885d4cc528d6cc34ff0ccb1b09
alescampos2511-lang <alescampos2511@gmail.com>
2026-06-27 01:57:30 -0600

    Arreglado y tipado todo el componente cases

---
+++ src/app/features/dashboard/roles/student/student.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  imports: [],
+  templateUrl: './student.html',
+  styleUrl: './student.scss',
+})
+export class Student {}
---
+++ src/app/features/dashboard/roles/teacher/teacher.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher',
+  imports: [],
+  templateUrl: './teacher.html',
+  styleUrl: './teacher.scss',
+})
+export class Teacher {}
